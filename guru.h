f8c33fe36f1f6bf702fb9a340da1553b98056bc4
kiii <muhammadrifkyalfajri@gmail.com>
2025-11-27 19:16:59 +0700

    Refactor guru.h for struct and typedef consistency

--- guru.h
+++ guru.h
@@ -15,7 +15,53 @@ typedef struct elmGuru *address;
 
 typedef elmGuruSLL {
     infotype info;
-    address next;
+    address next;#ifndef GURU_H_INCLUDED
+#define GURU_H_INCLUDED
+using namespace std;
+
+//guru sebagai parent
+
+struct guru{
+    string namaGuru;
+    string idGuru;
+};
+typedef guru infotype;
+typedef struct elmGuruSLL *addrSll;
+typedef struct elmGuruDLL *addrDll;
+//SLL
+
+struct elmGuruSLL {
+    infotype info;
+    addrSll next;
+    addrSll matKul; //untuk ke child matkul
+}
+
+struct ListGuruSLL{
+    addrSll first;
+};
+
+//DLL
+
+struct elmGuruDLL {
+    infotype info;
+    addrDll next;
+    addrDll prev;
+}
+
+struct ListGuruDLL{
+    addrDll first;
+    addrDll Last;
+};
+
+
+void createListGuruSll(ListGuruSLL &L);
+void createListGuruDll(ListGuruDLL &L);
+address createElmGuruSll(infotype x);
+address createElmGuruDll(infotype x);
+void insertFirstGuruSLL(ListGuruSLL &L, address p);
+
+#endif // GURU_H_INCLUDED
+
     address matKul; //untuk ke child matkul
 }
 
@@ -44,3 +90,4 @@ address createElmGuruDll(infotype x);
 void insertFirstGuruSLL(ListGuruSLL &L, address p);
 
 #endif // GURU_H_INCLUDED
+
