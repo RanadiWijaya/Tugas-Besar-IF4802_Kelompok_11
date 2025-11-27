5f37226e4c0480c809ff0588dfa4f11bf1507f4d
kiii <muhammadrifkyalfajri@gmail.com>
2025-11-27 19:17:41 +0700

    Update guru.h

--- guru.h
+++ guru.h
@@ -9,23 +9,6 @@ struct guru{
     string idGuru;
 };
 typedef guru infotype;
-typedef struct elmGuru *address;
-
-//SLL
-
-typedef elmGuruSLL {
-    infotype info;
-    address next;#ifndef GURU_H_INCLUDED
-#define GURU_H_INCLUDED
-using namespace std;
-
-//guru sebagai parent
-
-struct guru{
-    string namaGuru;
-    string idGuru;
-};
-typedef guru infotype;
 typedef struct elmGuruSLL *addrSll;
 typedef struct elmGuruDLL *addrDll;
 //SLL
@@ -61,33 +44,3 @@ address createElmGuruDll(infotype x);
 void insertFirstGuruSLL(ListGuruSLL &L, address p);
 
 #endif // GURU_H_INCLUDED
-
-    address matKul; //untuk ke child matkul
-}
-
-struct ListGuruSLL{
-    address first;
-};
-
-//DLL
-
-typedef elmGuruDLL {
-    infotype info;
-    address next;
-    address prev;
-}
-
-struct ListGuruDLL{
-    address first;
-    address Last;
-};
-
-
-void createListGuruSll(ListGuruSLL &L);
-void createListGuruDll(ListGuruDLL &L);
-address createElmGuruSll(infotype x);
-address createElmGuruDll(infotype x);
-void insertFirstGuruSLL(ListGuruSLL &L, address p);
-
-#endif // GURU_H_INCLUDED
-
