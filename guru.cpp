a5da324fc91451218c418fc310b8a6266e790c1a
kiii <muhammadrifkyalfajri@gmail.com>
2025-12-05 23:50:26 +0700

    Update guru.cpp

--- guru.cpp
+++ guru.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "guru.h"
-
+#include "matakuliah.h"
 void createListGuru(ListGuru &L){
     L.first = nullptr;
 }
@@ -65,7 +65,64 @@ void deleteAfterGuru(ListGuru &L, adrGuru prec, adrGuru &p){
     }
 }
 
-void SearchGuru(ListGuru L, adrGuru p);
-int hitungGuru(ListGuru L);
-void showAllData(ListGuru L);
-void showGuru(ListGuru L);
+void SearchGuru(ListGuru L, string nama, adrGuru p){
+    if(L.first != nullptr){
+        adrGuru q;
+        q = L.first;
+        while((q != nullptr) && (q->info.namaGuru != nama)){
+            q =q->next;
+        }
+    }else{
+        cout<<"Daftar Guru tidak ada. "<<endl;
+    }
+}
+int hitungGuru(ListGuru L){
+    int total = 0;
+    adrGuru q;
+    q = L.first;
+    while(q != nullptr){
+        total = total +1;
+        q =q->next;
+    }
+}
+void showAllData(ListGuru L){
+    if(L.first == nullptr){
+        cout<<"Data Guru tidak ada. "<<endl;
+    }else{
+        adrGuru p;
+        adrMatkul q;
+        p = L.first;
+        cout<<"===== DAFTAR GURU ====="<<endl;
+        while(p != nullptr){
+            cout<<"Nama: "<<p->info.namaGuru<<endl;
+            cout<<"ID: "<<p->info.idGuru<<endl;
+            cout<<"Kelas: "<<p->info.kelas<<endl;
+            cout<<"Mata Kuliah: ";
+            q = p->firstMatKul;
+            while(q != nullptr){
+                cout<< q->info.idMatkul<< ", ";
+                q = q->next;
+            }
+            cout<<"."<<endl;
+            cout<<"------------------------"<<endl;
+            p = p->next;
+        }
+    }
+}
+void showGuru(ListGuru L){
+    if(L.first == nullptr){
+        cout<<"Data Guru tidak ada. "<<endl;
+    }else{
+        adrGuru p;
+        p = L.first;
+        cout<<"===== GURU ====="<<endl;
+        while(p != nullptr){
+            cout<<"Nama: "<<p->info.namaGuru<<endl;
+            cout<<"ID: "<<p->info.idGuru<<endl;
+            cout<<"Kelas: "<<p->info.kelas<<endl;
+            cout<<"."<<endl;
+            cout<<"------------------------"<<endl;
+            p = p->next;
+        }
+    }
+}
