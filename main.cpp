b627b2c523bf834dea91795609ce203eea10746a
kiii <muhammadrifkyalfajri@gmail.com>
2025-12-06 22:33:12 +0700

    Implement main menu with options for users

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include "main.h"
 
+using namespace std;
+
+int main()
+{
+    int option=-99;
+    while (option != 0) {
+      
+        ListGuru LG;
+        adrGuru AG;
+
+        system("cls");
+        cout << "============ Menu ============ " << endl;
+        cout << "|| 1. Test Case             ||" << endl;
+        cout << "|| 2. Study Case            ||" << endl;
+        cout << "|| 0. exit                  ||" << endl;
+        cout << "============================== " << endl;
+        cout << "Choose your option : ";
+        cin >> option;
+        switch(option) {
+           case 1  :
+              cout << "you choose option 1" << endl;
+              menuAdmin(LG);
+              break;
+           case 2  :
+              cout << "you choose option 2" << endl;
+              menuUser(LG);
+              break;
+
+           default :
+              exit(1);
+        }
+    }
+
+
+    return 0;
+}
