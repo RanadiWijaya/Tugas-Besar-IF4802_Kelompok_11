7d5de32e0407d5cdf594fe62f0a576314992a082
kiii <muhammadrifkyalfajri@gmail.com>
2025-12-12 09:11:26 +0700

    bagian insert after, delete first, last, show matkul

--- matakuliah_103012430036.cpp
+++ matakuliah_103012430036.cpp
@@ -1,40 +1,6 @@
 #include "matakuliah.h"
 #include <iostream>
 
-void createListMatkul(ListMatkul &L) {
-    L.first = nullptr;
-    L.last = nullptr;
-};
-
-adrMatkul createElmMatkul(infotype x) {
-    adrMatkul p = new elmMataKuliah;
-    p->info = x;
-    p->next = nullptr;
-    p->prev = nullptr;
-    return p;
-};
-
-void isnertFirstMatkul(ListMatkul &L, adrMatkul p) {
-    if (L.first == nullptr) {
-        L.first = p;
-        L.last = p;
-    } else {
-        p->next = L.first;
-        L.first->prev = p;
-        L.first = p;
-    }
-};
-
-void insertLastMatkul(ListMatkul &L, adrMatkul p) {
-    if (L.first == nullptr) {
-        L.first = p;
-        L.last = p;
-    } else {
-        L.last->next = p;
-        p->prev = L.last;
-        L.last = p;
-    }
-};
 
 void insertAfterMatkul(ListMatkul &L, adrMatkul prec, adrMatkul p) {
     if (prec != nullptr) {
@@ -78,41 +44,6 @@ void deleteLastMatkul(ListMatkul &L, adrMatkul &p){
     };
 };
 
-void deleteAfteMatkul(ListMatkul &L, adrMatkul prec, adrMatkul &p){
-    if (prec != nullptr && prec -> next != nullptr){
-        p = prec -> next;
-        prec -> next = p -> next;
-        if (p -> next != nullptr){
-            p -> next -> prev = prec;
-        } else {
-            L.last = prec; // Update last if menghapus element terakhir
-        };
-        p -> next = nullptr;
-        p -> prev = nullptr;
-    };
-};
-
-adrMatkul SearchMatkul(ListMatkul L, string idMatlkul){
-    adrMatkul p = L.first;
-    while (p != nullptr){
-        if (p -> info.idMatkul == idMatlkul){
-            return p;
-        };
-        p = p -> next;
-    };
-    return nullptr;
-};
-
-int hitungMatkul(ListMatkul L){
-    int count = 0;
-    adrMatkul p = L.first;
-    while (p != nullptr){
-        count++;
-        p = p -> next;
-    };
-    return count;
-};
-
 void showMatkul(ListMatkul L){
     adrMatkul p = L.first;
     if(p == nullptr){
@@ -127,3 +58,4 @@ void showMatkul(ListMatkul L){
         }
     }
 }
+
