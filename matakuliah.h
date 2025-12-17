6f73f490433bf8d43ed8c0c2d05f6d451536f078
kiii <muhammadrifkyalfajri@gmail.com>
2025-12-17 23:25:53 +0700

    benerin adt fungsi procedure

--- matakuliah.h
+++ matakuliah.h
@@ -9,12 +9,16 @@ using namespace std;
 struct mataKuliah{
     string namaMatkul;
     string idMatkul;
-    int jam;
+    string jam; //berapa jam matakuliah tersebut(sks)
 };
 
 
 typedef mataKuliah infotype;
 typedef struct elmMataKuliah *adrMatkul;
+
+typedef struct elmGuru *adrGuru;
+struct elmGuru;
+
 
 struct elmMataKuliah {
     infotype info;
@@ -27,18 +31,17 @@ struct ListMatkul{
     adrMatkul last;
 };
 
-void createListMatkul(ListMatkul &L);
+
 adrMatkul createElmMatkul(infotype x);
-void insertFirstMatkul(ListMatkul &L, adrMatkul p);
-void insertLastMatkul(ListMatkul &L, adrMatkul p);
-void insertAfterMatkul(ListMatkul &L, adrMatkul prec, adrMatkul p);
-void deleteFirstMatkul(ListMatkul &L, adrMatkul &p);
-void deleteLastMatkul(ListMatkul &L, adrMatkul &p);
-void deleteAfterMatkul(ListMatkul &L, adrMatkul prec, adrMatkul &p);
-void SearchMatkul(ListMatkul L, adrMatkul &p);
-int hitungMatkul(ListMatkul L);
-void showMatkul(ListMatkul L);
+void insertFirstMatkul(adrGuru &g, adrMatkul p);
+void insertLastMatkul(adrGuru &g, adrMatkul p);
+void insertAfterMatkul(adrGuru &g, adrMatkul prec, adrMatkul p);
+void deleteFirstMatkul(adrGuru &g, adrMatkul &p);
+void deleteLastMatkul(adrGuru &g, adrMatkul &p);
+void deleteAfterMatkul(adrGuru &g, adrMatkul prec, adrMatkul &p);
+adrMatkul SearchMatkul(adrGuru g, string idMatkul);
+int hitungMatkul(adrGuru g);
+void showMatkul(adrGuru g);
 
 #endif // MATAKULIAH_H_INCLUDED
-
 
